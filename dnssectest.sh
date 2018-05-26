4dead081387064d3f00e520525cfe7725f9a0055
Russell Morris <rmorris@rkmorris.us>
2018-05-25 19:57:51 -0500

    Add script for DNSSEC testing

---
+++ dnssectest.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash
+
+command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
+{ command -v drill > /dev/null && dig=drill; } || { command -v kdig > /dev/null && dig=kdig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
+
+
+
+NAMESERVERS=`cat /etc/resolv.conf | grep ^nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
+
+PROVIDERS="
+1.1.1.1#cloudflare 
+4.2.2.1#level3 
+8.8.8.8#google 
+9.9.9.9#quad9 
+80.80.80.80#freenom 
+208.67.222.123#opendns 
+199.85.126.20#norton 
+185.228.168.168#cleanbrowsing 
+77.88.8.7#yandex 
+176.103.130.132#adguard 
+156.154.70.3#neustar 
+8.26.56.26#comodo
+"
+
+# Domains to test. Duplicated domains are ok
+DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+
+round()
+{
+echo $(printf %.$2f $(echo "scale=$2;(((10^$2)*$1)+0.5)/(10^$2)" | bc))
+};
+
+
+totaldomains=0
+printf "%-18s" ""
+for d in $DOMAINS2TEST; do
+    totaldomains=$((totaldomains + 1))
+    printf "%-8s" "test$totaldomains"
+done
+printf "%-8s" "Average"
+echo ""
+
+
+for p in $NAMESERVERS $PROVIDERS; do
+    pip=${p%%#*}
+    pname=${p##*#}
+    ftime=0
+
+    printf "%-18s" "$pname"
+    for d in $DOMAINS2TEST; do
+        digResult=`$dig +retry=3 +time=2 +stats +dnssec @$pip $d 2>/dev/null | grep "From " | cut -d " " -f 5`
+	strLen=${#digResult}
+        if [ $strLen -eq 0 ]; then
+	        #timeout = 1000ms, due to +time=2 argument above
+	        ttime=2000
+	else
+		ttime=`echo $(round $digResult 0)`
+	fi
+	#echo $digResult, $strLen, $ttime
+
+        printf "%-8s" "$ttime ms"
+        ftime=$((ftime + ttime))
+    done
+    avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
+
+    echo "  $avg"
+done
+
+
+exit 0;
